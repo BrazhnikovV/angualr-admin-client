3f9291d3cf0e64a698c0f58f2cbb473f32388663
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-04-21 13:14:17 +0300

    Добавил компоненты для редактируемых сущностей приложения и навигацию для этих компонентов.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { TopMenuComponent } from './top-menu/top-menu.component';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuComponent } from './admin/menu/menu.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { HeaderComponent } from './admin/header/header.component';
+import { SliderComponent } from './admin/slider/slider.component';
+import { OrdersComponent } from './admin/orders/orders.component';
 import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { CatalogComponent } from './admin/catalog/catalog.component';
+import { PartnersComponent } from './admin/partners/partners.component';
 
 const appRoutes: Routes = [
-  {path: 'UI/part1/Details', component: AppComponent}
+  { path: 'menu',     component: MenuComponent },
+  { path: 'header',   component: HeaderComponent },
+  { path: 'slider',   component: SliderComponent },
+  { path: 'orders',   component: OrdersComponent },
+  { path: 'catalog',  component: CatalogComponent },
+  { path: 'partners', component: PartnersComponent }
+  // { path: '**', component: Page404Component },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    SliderComponent,
+    OrdersComponent,
     TopMenuComponent,
-    LeftMenuComponent
+    CatalogComponent,
+    LeftMenuComponent,
+    PartnersComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/left-menu/left-menu.component.ts
+++ src/app/left-menu/left-menu.component.ts
@@ -15,29 +15,35 @@ export class LeftMenuComponent implements OnInit {
       {
         label: 'Каталог',
         icon: 'pi pi-pw pi-list',
+        url: '/catalog'
       },
       {
         label: 'Меню',
         icon: 'pi pi-pw pi-bars',
+        url: '/menu'
         // items: [
           // {label: 'Delete', icon: 'pi pi-fw pi-trash'},
         // ]
       },
       {
         label: 'Партнеры',
-        icon: 'pi pi-pw pi-user-plus'
+        icon: 'pi pi-pw pi-user-plus',
+        url: '/partners'
       },
       {
         label: 'Шапка',
         icon: 'pi pi-pw pi-eject',
+        url: '/header'
       },
       {
         label: 'Слайдер',
         icon: 'pi pi-pw pi-camera',
+        url: '/slider'
       },
       {
         label: 'Заказы',
         icon: 'pi pi-pw pi-dollar',
+        url: '/orders'
       }
     ];
   }
