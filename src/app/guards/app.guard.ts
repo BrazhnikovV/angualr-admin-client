a989aff7724ffb840beeee3ce7284a4c30f74956
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-05-09 23:14:14 +0300

    Поправил стража + докблоки.

--- src/app/guards/app.guard.ts
+++ src/app/guards/app.guard.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import {AuthService} from '../services/auth.service';
 
+/**
+ * @class - AppGuard
+ * @classdesc - главный страж приложения
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class AppGuard implements CanActivate {
+
+  /**
+   * constructor
+   * @param authService - сервис атентификации
+   */
+  constructor ( private authService: AuthService ) {}
+
+  /**
+   * canActivate
+   * @override
+   * @param next - ActivatedRouteSnapshot
+   * @param state - RouterStateSnapshot
+   */
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.authService.getIsLogged();
   }
 }
