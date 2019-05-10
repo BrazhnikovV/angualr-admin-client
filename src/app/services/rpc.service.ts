eb14ec6b729fe6681d22ed6b00840c0f65947378
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-05-10 18:47:23 +0300

    Описание и докблоки для сервиса RCP.

--- src/app/services/rpc.service.ts
+++ src/app/services/rpc.service.ts
@@ -1,11 +1,11 @@
+import { Order } from '../models/order';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { catchError, tap } from 'rxjs/operators';
-import { Observable, throwError} from 'rxjs';
-import { Category } from '../models/category';
 import { Product } from '../models/product';
 import { Partner } from '../models/partner';
-import { Order } from '../models/order';
+import { Category } from '../models/category'
+import { Observable, throwError} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
 
 /**
  * @class - RpcService
@@ -26,8 +26,6 @@ export class RpcService {
    */
   private token = '?access-token=9zfb5fKB4f9rAC-b_HEawN6dSGLO0Krh';
 
-  private categories: Category[];
-
   /**
    * constructor - конструктор
    * @param http - объект для работы с http
@@ -35,40 +33,22 @@ export class RpcService {
   constructor( private http: HttpClient) {}
 
   /**
-   * getData - получить данные от сервера
-   * @param String: requestType - тип запроса
-   * @param String: route - маршрут запроса
-   * @param Array<string> data - массив данных для post - запроса
-   */
-  public getData( requestType: string, route: string, data: Array<string> = [] ): Observable<{} | HttpClient> {
-
-    console.log(this.apiUrl + route + this.token);
-
-    if ( requestType === 'post' ) {
-      return this.http.post( this.apiUrl + route + this.token, data ).pipe(
-        tap(response => {}),
-        catchError(response => this.handleError(data))
-      );
-    } else {
-      return this.http.get( this.apiUrl + route + this.token ).pipe(
-        tap(response => {}),
-        catchError(response => this.handleError(data))
-      );
-    }
-  }
-
-  /**
-   * getCategories - получить данные от сервера
+   * getCategories - получить список категорий
+   * @return Observable<any> | throwError( error )
    */
   public getCategories(): Observable<any> {
     return this.http.get<Category[]>( this.apiUrl + 'categories' + this.token ).pipe(
       tap(response => {}),
-      catchError(response => this.handleError( response ) )
+      catchError(error => {
+        return throwError( error );
+      })
     );
   }
 
   /**
-   * postCategory -
+   * postCategory - создать категорию
+   * @param data: Array<string> - массив данных для отправки на сервер
+   * @return Observable<any> | throwError( error )
    */
   public postCategory(  data: Array<string> = [] ): Observable<any> {
     return this.http.post<Category[]>( this.apiUrl + 'categories' + this.token, data ).pipe(
@@ -80,7 +60,22 @@ export class RpcService {
   }
 
   /**
-   * postProduct -
+   * getProducts - получить список продуктов
+   * @return Observable<any> | throwError( error )
+   */
+  public getProducts(): Observable<any> {
+    return this.http.get<Product[]>( this.apiUrl + 'products' + this.token ).pipe(
+      tap(response => {}),
+      catchError(error => {
+        return throwError( error );
+      })
+    );
+  }
+
+  /**
+   * postProduct - создать продукт
+   * @param data: Array<string> - массив данных для отправки на сервер
+   * @return Observable<any> | throwError( error )
    */
   public postProduct(  data: Array<string> = [] ): Observable<any> {
     return this.http.post<Product[]>( this.apiUrl + 'products' + this.token, data ).pipe(
@@ -92,27 +87,22 @@ export class RpcService {
   }
 
   /**
-   * getProducts - получить данные от сервера
-   */
-  public getProducts(): Observable<any> {
-    return this.http.get<Product[]>( this.apiUrl + 'products' + this.token ).pipe(
-      tap(response => {}),
-      catchError(response => this.handleError( response ) )
-    );
-  }
-
-  /**
-   * getPartners - получить данные от сервера
+   * getPartners - получить список партнеров
+   * @return Observable<any> | throwError( error )
    */
   public getPartners(): Observable<any> {
     return this.http.get<Partner[]>( this.apiUrl + 'partners' + this.token ).pipe(
       tap(response => {}),
-      catchError(response => this.handleError( response ) )
+      catchError(error => {
+        return throwError( error );
+      })
     );
   }
 
   /**
-   * postProduct -
+   * postPartner - создать партнера
+   * @param data: Array<string> - массив данных для отправки на сервер
+   * @return Observable<any> | throwError( error )
    */
   public postPartner(  data: Array<string> = [] ): Observable<any> {
     return this.http.post<Partner[]>( this.apiUrl + 'partners' + this.token, data ).pipe(
@@ -125,20 +115,14 @@ export class RpcService {
 
   /**
    * getOrders - получить данные от сервера
+   * @return Observable<any> | throwError( error )
    */
   public getOrders(): Observable<any> {
     return this.http.get<Order[]>( this.apiUrl + 'orders' + this.token ).pipe(
       tap(response => {}),
-      catchError(response => this.handleError( response ) )
+      catchError(error => {
+        return throwError( error );
+      })
     );
-  }
-
-  /**
-   * handleError - обработать ошибку получения/отправки данных
-   * @param data - данные об ошибке
-   */
-  private handleError( data: any ) {
-    console.log('error data:', data);
-    return Observable.create();
   }
 }
