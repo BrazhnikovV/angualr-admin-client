5a914fb9a88905afa1b37e6e3ad75df07330e465
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-05-10 17:56:54 +0300

    Расширен компонент отображения сообщений валидатора.

--- src/app/admin/products/products-create/products-create.component.ts
+++ src/app/admin/products/products-create/products-create.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { RpcService } from 'src/app/services/rpc.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ValidatorMessageComponent } from 'src/app/validator-message/validator-message.component';
 
 /**
  * @class - ProductsCreateComponent
@@ -13,14 +17,98 @@ export class ProductsCreateComponent implements OnInit {
 
   /**
    * @access private
-   * @var false checked
+   * @var viewChildren: QueryList<ValidatorMessageComponent> - список компонентов
+   * для отображения сообщений валидатора
    */
-  private checked = false;
+  @ViewChildren( ValidatorMessageComponent )
+  private viewChildren: QueryList<ValidatorMessageComponent>;
+
+  /**
+   *  @access private
+   *  @var errors: [] - массив ошибок, полученных при аутентификации
+   */
+  private errors: [];
+
+  /**
+   *  @access private
+   *  @var productForm: FormGroup - группа валидируемых полей
+   */
+  private productForm: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4 ),
+      Validators.maxLength(128 )
+    ]),
+    description: new FormControl('' ,[
+      Validators.required,
+      Validators.minLength(4 ),
+      Validators.maxLength(512 )
+    ]),
+    price: new FormControl('' ,[
+      Validators.required,
+      Validators.pattern('[0-9]*'),
+      Validators.min(1 ),
+      Validators.max(10000 )
+    ]),
+    code: new FormControl('' ,[
+      Validators.required,
+      Validators.minLength(1 ),
+      Validators.maxLength(10000 )
+    ]),
+    hidden: new FormControl('' ,[
+      Validators.required,
+      Validators.minLength( 0 ),
+      Validators.maxLength(1 )
+    ])
+  });
 
   /**
    * constructor
+   * @param rpcService: RpcService - сервис для выполнения запроса
+   * @param router -
    */
-  constructor() {}
+  constructor( private rpcService: RpcService, private router: Router ) {}
 
+  /**
+   * onSubmit - перехватываем события откравки формы
+   * @return void
+   */
+  onSubmit() {
+    console.log('### CategoryCreateComponent => onSubmit()');
+    this.rpcService.postProduct( this.productForm.value ).subscribe(
+      response => { this.handleResponse( response ) },error => { this.handleError( error ) }
+    );
+  }
+
+  /**
+   * inputChange - перехватываем событие набора символов в текстовых полях
+   * @return void
+   */
+  inputChange() {
+    this.viewChildren.forEach( child => child.ngOnChanges() );
+  }
+
+  /**
+   * ngOnInit
+   */
   ngOnInit() {}
+
+  /**
+   * handleError - обработать ошибку получения/отправки данных
+   * @param error - данные об ошибке
+   */
+  private handleError( response: any ) {
+    console.log('### CategoryCreateComponent => handleError()');
+    this.errors = response.error;
+  }
+
+  /**
+   * handleResponse -
+   * @param response - данные об ошибке
+   */
+  private handleResponse( response: any ) {
+    console.log('### CategoryCreateComponent => handleResponse()');
+    console.log('response data:', response);
+    this.router.navigate(['/products/list']);
+  }
 }
--- src/app/validator-message/validator-message.component.ts
+++ src/app/validator-message/validator-message.component.ts
@@ -32,9 +32,9 @@ export class ValidatorMessageComponent implements OnInit {
    *  @var msgCnf: {} - небор текстовых сообщений для различных видов валидации
    */
   private msgCnf: {} = {
-    required: 'Field is required',
-    email:    'Field should contain e-mail',
-    pattern:  'Field does not match to pattern'
+    required: ' Field is required',
+    email:    ' Field should contain e-mail',
+    pattern:  ' Field does not match to pattern'
   }
 
   /**
@@ -60,10 +60,22 @@ export class ValidatorMessageComponent implements OnInit {
 
     this.msgs = [];
     if ( field.errors.hasOwnProperty( 'minlength' ) ) {
-      this.msgCnf['minlength'] = `Minimum length ${ field.errors.minlength.requiredLength }`;
+      this.msgCnf['minlength'] = ` Minimum length ${ field.errors.minlength.requiredLength }`;
     }
     if ( field.errors.hasOwnProperty( 'maxlength' ) ) {
-      this.msgCnf['maxlength'] = `Maximum length ${ field.errors.maxlength.requiredLength }`;
+      this.msgCnf['maxlength'] = ` Maximum length ${ field.errors.maxlength.requiredLength }`;
+    }
+    if ( field.errors.hasOwnProperty( 'min' ) ) {
+      this.msgCnf['min'] = ` Min number ${ field.errors.min.min }`;
+    }
+    if ( field.errors.hasOwnProperty( 'max' ) ) {
+      this.msgCnf['max'] = ` Max number length ${ field.errors.max.max }`;
+    }
+
+    if ( field.errors.hasOwnProperty( 'pattern' ) ) {
+      if ( field.errors.pattern.requiredPattern === '^[0-9]*$' ) {
+        this.msgCnf['pattern'] = ` There must be a number: ${ field.errors.pattern.requiredPattern }`;
+      }
     }
 
     Object.keys( field.errors ).forEach( ( error: string ) => {
